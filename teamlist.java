4ee0c2d17e3fbc70ab8e44606db359155ff8e29f
Clay OL <clay.oshiroleavitt@gmail.com>
2019-01-10 14:51:47 -0500

    Added name print line for Clay Oshiro-Leavitt

--- teamlist.java
+++ teamlist.java
@@ -2,5 +2,6 @@ public class teamlist{
     public static void main(String[] args)
     {
         System.out.println("Jonathan Sanchez");
+        System.out.println("Clay Oshiro-Leavitt");
     }
 }
