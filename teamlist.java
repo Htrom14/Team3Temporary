173e6c53a70b36389d88ee3d1aab78ab02f448b2
cbell2 <cbell2@wpi.edu>
2019-01-10 14:49:20 -0500

    Chris Bell added

--- teamlist.java
+++ teamlist.java
@@ -2,6 +2,7 @@ public class teamlist{
     public static void main(String[] args)
     {
         System.out.println("Jonathan Sanchez");
+        System.out.println("Chris Bell");
         System.out.println("James Petullo");
         System.out.println("Hannah Baez");
     }
